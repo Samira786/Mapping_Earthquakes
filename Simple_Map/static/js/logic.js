0a009b9a609ae802ba3d20d7786c76053a09745a
samira <samben@Sams-MacBook-Air.local>
2020-04-12 16:04:21 -0700

    trial

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,5 +1,12 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
+let map= L.map('mapid').setView([34.0522, -118.2437],14);
+//  Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437],{
+	radius: 300,
+	color: "black",
+	fillColor: '#ffffa1'
+ }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
